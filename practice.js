dbcf6fd73bd3da97f620554efa88eee948e51331
Anzusa17 <43911406+Anzusa17@users.noreply.github.com>
2019-02-09 09:50:54 -0800

    Changes
    Changes

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
 let newHeading = document.createElement("h2");
 newHeading.innerHTML = "FUN!";
-let about = document.getElementById("About Me");
+let about = document.getElementById("About");
 about.appendChild(newHeading);
